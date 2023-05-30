a32d5f7e9ab168f2405f9d662757aba5b97af48d
Anthony Lacruz <lacruzanthony20@gmail.com>
2023-05-30 10:21:36 -0500

    adding images to the main page

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+const PlaceHolderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceHolderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+F;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 16,
   },
 });
